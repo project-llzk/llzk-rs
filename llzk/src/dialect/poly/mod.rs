dda12899b5229489865d3175a3acf5f9249dba92
Tim Hoffman <timothy.hoffman@veridise.com>
2026-02-04 14:50:26 -0600

    refactor: handle `TVarType` like the others

--- llzk/src/dialect/poly/mod.rs
+++ llzk/src/dialect/poly/mod.rs
@@ -4,6 +4,7 @@ pub mod ops;
 pub mod r#type;
 pub use ops::{applymap, read_const, unifiable_cast};
 pub use ops::{is_applymap_op, is_read_const_op, is_unifiable_cast_op};
+pub use r#type::{TVarType, is_type_variable};
 
 use llzk_sys::mlirGetDialectHandle__llzk__polymorphic__;
 use melior::dialect::DialectHandle;
@@ -11,4 +12,9 @@ use melior::dialect::DialectHandle;
 /// Returns a handle to the `poly` dialect.
 pub fn handle() -> DialectHandle {
     unsafe { DialectHandle::from_raw(mlirGetDialectHandle__llzk__polymorphic__()) }
+}
+
+/// Exports the common types of the poly dialect.
+pub mod prelude {
+    pub use super::r#type::{TVarType, is_type_variable};
 }
