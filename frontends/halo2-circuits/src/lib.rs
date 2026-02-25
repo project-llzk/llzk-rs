145088f63d4dda571a0f71e4924e832332e7d1fc
Daniel Dominguez <daniel@veridise.com>
2026-02-25 14:47:53 +0100

    Remove halo2 frontend

--- frontends/halo2-circuits/src/lib.rs
+++
@@ -1,5 +0,0 @@
-//! Collection of circuits used for testing the Halo2 frontend.
-
-pub mod fibonacci;
-pub mod lookup;
-pub mod mul;
--- frontends/halo2/src/ir/expr.rs
+++
@@ -1,8 +0,0 @@
-//! Structs for handling expressions.
-
-pub mod aexpr;
-pub mod bexpr;
-
-/// Reexported to avoid compilation errors.
-pub use aexpr::*;
-pub use bexpr::*;
