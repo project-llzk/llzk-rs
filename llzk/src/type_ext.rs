1b2e72da19af2727b69e3f685b720b8e31bb2a95
Daniel Dominguez <daniel@veridise.com>
2026-05-28 19:31:37 +0200

    Improve iterators

--- llzk/src/type_ext.rs
+++ llzk/src/type_ext.rs
@@ -3,47 +3,117 @@
 use melior::ir::r#type::FunctionType;
 
 mod function_type_ext {
+    use std::iter::FusedIterator;
+
     use melior::ir::{Type, r#type::FunctionType};
 
     /// Iterator over the inputs of a function type.
     #[derive(Debug)]
     pub struct InputsIter<'ctx> {
-        pub(super) fn_type: FunctionType<'ctx>,
-        pub(super) count: usize,
+        fn_type: FunctionType<'ctx>,
+        start: usize,
+        end: usize,
+    }
+
+    impl<'ctx> InputsIter<'ctx> {
+        pub(super) fn new(fn_type: FunctionType<'ctx>) -> Self {
+            Self {
+                fn_type,
+                start: 0,
+                end: fn_type.input_count(),
+            }
+        }
     }
 
     impl<'ctx> Iterator for InputsIter<'ctx> {
         type Item = Type<'ctx>;
 
         fn next(&mut self) -> Option<Self::Item> {
-            if self.count >= self.fn_type.input_count() {
+            if self.start >= self.end {
                 return None;
             }
-            let count = self.count;
-            self.count += 1;
-            self.fn_type.input(count).ok()
+            let n = self.start;
+            self.start += 1;
+            Some(self.fn_type.input(n).unwrap())
+        }
+
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            if self.start >= self.end {
+                return (0, Some(0));
+            }
+            let size = self.end - self.start;
+            (size, Some(size))
         }
     }
+
+    impl ExactSizeIterator for InputsIter<'_> {}
+
+    impl DoubleEndedIterator for InputsIter<'_> {
+        fn next_back(&mut self) -> Option<Self::Item> {
+            if self.start >= self.end {
+                return None;
+            }
+            let n = self.end;
+            self.end -= 1;
+            Some(self.fn_type.input(n).unwrap())
+        }
+    }
+
+    impl FusedIterator for InputsIter<'_> {}
 
     /// Iterator over the results of a function type.
     #[derive(Debug)]
     pub struct ResultsIter<'ctx> {
-        pub(super) fn_type: FunctionType<'ctx>,
-        pub(super) count: usize,
+        fn_type: FunctionType<'ctx>,
+        start: usize,
+        end: usize,
+    }
+
+    impl<'ctx> ResultsIter<'ctx> {
+        pub(super) fn new(fn_type: FunctionType<'ctx>) -> Self {
+            Self {
+                fn_type,
+                start: 0,
+                end: fn_type.result_count(),
+            }
+        }
     }
 
     impl<'ctx> Iterator for ResultsIter<'ctx> {
         type Item = Type<'ctx>;
 
         fn next(&mut self) -> Option<Self::Item> {
-            if self.count >= self.fn_type.result_count() {
+            if self.start >= self.end {
                 return None;
             }
-            let count = self.count;
-            self.count += 1;
-            self.fn_type.result(count).ok()
+            let n = self.start;
+            self.start += 1;
+            Some(self.fn_type.result(n).unwrap())
+        }
+
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            if self.start >= self.end {
+                return (0, Some(0));
+            }
+            let size = self.end - self.start;
+            (size, Some(size))
         }
     }
+
+    impl ExactSizeIterator for ResultsIter<'_> {}
+
+    impl DoubleEndedIterator for ResultsIter<'_> {
+        fn next_back(&mut self) -> Option<Self::Item> {
+            if self.start >= self.end {
+                return None;
+            }
+            let n = self.end;
+            self.end -= 1;
+            Some(self.fn_type.result(n).unwrap())
+        }
+    }
+
+    impl FusedIterator for ResultsIter<'_> {}
 
     /// Sealed trait to avoid additional implementations of `FunctionTypeExt`.
     pub trait FunctionTypeExtSealed {}
@@ -60,17 +130,11 @@ pub trait FunctionTypeExt<'ctx>: function_type_ext::FunctionTypeExtSealed {
 
 impl<'ctx> FunctionTypeExt<'ctx> for FunctionType<'ctx> {
     fn inputs(&self) -> function_type_ext::InputsIter<'ctx> {
-        function_type_ext::InputsIter {
-            fn_type: *self,
-            count: 0,
-        }
+        function_type_ext::InputsIter::new(*self)
     }
 
     fn results(&self) -> function_type_ext::ResultsIter<'ctx> {
-        function_type_ext::ResultsIter {
-            fn_type: *self,
-            count: 0,
-        }
+        function_type_ext::ResultsIter::new(*self)
     }
 }
 
